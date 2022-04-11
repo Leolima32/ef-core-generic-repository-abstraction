c5424e2a96011e339b59d50639c29c84624f5eb4
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2022-04-10 22:54:32 -0300

    Project Structure

---
+++ tests/GenericRepository.EntityFrameworkCore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GenericRepository.EntityFrameworkCore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
