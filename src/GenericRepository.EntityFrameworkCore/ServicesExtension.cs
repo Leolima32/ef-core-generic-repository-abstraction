9fed196dacbf7a130c09daa6cb0a145fd1543712
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2022-04-13 12:42:08 -0300

    Unify projects

--- src/GenericRepository.EntityFrameworkCore/ServicesExtension.cs
+++ src/GenericRepository.EntityFrameworkCore/ServicesExtension.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace GenericRepository.EntityFrameworkCore.ServiceInjection
+namespace GenericRepository.EntityFrameworkCore
 {
     public static class ServicesExtension
     {
