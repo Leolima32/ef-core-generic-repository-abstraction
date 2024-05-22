bf6fede1e87980e5c19f9b6c6ae2774ee0cc9e31
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2024-05-22 10:58:51 -0300

    enable configuration

--- src/GenericRepository.EntityFrameworkCore/ServicesExtension.cs
+++ src/GenericRepository.EntityFrameworkCore/ServicesExtension.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LF.GenericRepository.EntityFrameworkCore
 {
     public static class ServicesExtension
     {
-        public static void AddGenericRepositorySqlServer<T>(this IServiceCollection services, string connectionString) where T : GenericDbContext
+        public static void AddGenericRepositorySqlServer<T>(this IServiceCollection services, string? connectionString, Action<SqlServerDbContextOptionsBuilder>? sqlServerOptionsAction = null) where T : GenericDbContext
         {
             services.AddDbContext<T>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlServer(connectionString, sqlServerOptionsAction));
         }
 
         public static void CreateDatabaseIfNotExists(this IServiceCollection services, GenericDbContext context)
