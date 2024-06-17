f05c4a4befa69174a0de4b2539a7c7d8df349f2d
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2024-06-17 16:17:23 -0300

    include savechangesasync override method

--- src/GenericRepository.EntityFrameworkCore/GenericDbContext.cs
+++ src/GenericRepository.EntityFrameworkCore/GenericDbContext.cs
@@ -35,5 +35,33 @@ namespace LF.GenericRepository.EntityFrameworkCore
             }
             return base.SaveChanges();
         }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreatedDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("CreatedDate").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("CreatedDate").IsModified = false;
+                }
+            }
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("LastUpdated") != null))
+            {
+                entry.Property("LastUpdated").CurrentValue = DateTime.Now;
+            }
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("Active") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("Active").CurrentValue = true;
+                }
+            }
+            return base.SaveChangesAsync();
+        }
     }
 }
